436850b9bd096c118586520c9d2122cc54a6fdd9
Moon Hyunbin <125638803+cleanbin98@users.noreply.github.com>
2024-05-22 15:07:31 +0900

    Update ObjectDetection.py

--- ObjectDetection/ObjectDetection.py
+++ ObjectDetection/ObjectDetection.py
@@ -3,10 +3,12 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+#머신 러닝 학습을 위한 학습 라벨 파일을 가져옴.
 with open("./mscoco_complete_label_map.pbtxt", "rt") as f:
     pb_classes = f.read().rstrip("\n").split("\n")
     classes_label = dict()
 
+    #학습 라벨 파일에 접근해서 필요한 데이터를 가져옴.
     for i in range(0, len(pb_classes), 5):
         pb_classId = int(re.findall("\d+", pb_classes[i + 2])[0])
         pattern = 'display_name: "(.*?)"'
@@ -21,7 +23,7 @@ while True:
 
     if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
         break
-
+    #화면 출력을 위한 변수 선언.
     input_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     input_img = cv2.resize(frame, dsize=(1080,720), interpolation=cv2.INTER_LINEAR)
     input_tensor = tf.convert_to_tensor(input_img)
@@ -34,6 +36,7 @@ while True:
     boxes = output_dict["detection_boxes"][0]
 
     height, width, _ = input_img.shape
+    #70% 이상 정확해야지 라벨의 데이터를 화면에 보여줌.
     for idx, score in enumerate(scores):
         if score > 0.7:
             class_id = int(classes[idx])
